7ff509b1e921ec5a199539161ed23e35ad9da476
noahvanhaute <noah.vanhaute@proton.me>
2025-04-02 13:42:05 +0200

    added tikz neovim snippets

---
+++ .config/nvim/luasnippets/tex/tikz.lua
@@ -0,0 +1,23 @@
+-- all snippets related to TikZ in LaTeX
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
+ls.add_snippets("tex", {
+	-- tikzpicture environment
+	s(
+		"tk",
+		fmta(
+			[=[
+			\begin{tikzpicture}
+				<>
+			\end{tikzpicuture}
+			]=],
+			{ i(1) }
+		)
+	),
+})
