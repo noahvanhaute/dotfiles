69c70fac161676be912bceeb60265af8fd980862
noahvanhaute <noah.vanhaute@proton.me>
2025-04-09 15:52:51 +0200

    added beamer snippets

---
+++ .config/nvim/luasnippets/tex/beamer.lua
@@ -0,0 +1,23 @@
+-- all snippets related to math in LaTeX
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local fmta = require("luasnip.extras.fmt").fmta
+local rep = require("luasnip.extras").rep
+
+ls.add_snippets("tex", {
+	-- frame environment
+	s(
+		"fr",
+		fmta(
+			[=[
+            \begin{frame}{<>}{<>}
+                <>
+            \end{frame}
+            ]=],
+			{ i(1), i(2), i(3) }
+		)
+	),
+})
