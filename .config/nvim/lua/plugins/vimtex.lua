5d17e67c8c6a4dbfb837da27e7869b86d1dc4083
noahvanhaute <noah.vanhaute@proton.me>
2025-04-26 14:46:08 +0200

    fixed fixed comment

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -10,7 +10,7 @@ return {
 			vim.g.vimtex_view_method = "zathura"
 		end
 
-		-- open QuickFix for warnings only
+		-- don't open QuickFix for warnings only
 		vim.g.vimtex_quickfix_open_on_warning = false
 
 		-- fzf-lua for ToC
