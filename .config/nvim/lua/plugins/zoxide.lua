a94e1c191457e6cd03f7054dd87e18e9d0c8b9de
noahvanhaute <noah.vanhaute@proton.me>
2025-04-26 14:39:15 +0200

    have zoxide.vim use fzf-lua instead of fzf.vim

--- .config/nvim/lua/plugins/zoxide.lua
+++ .config/nvim/lua/plugins/zoxide.lua
@@ -3,4 +3,8 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>cd", ":Z ", { desc = "[C]hange [D]irectory with zoxide" })
 	end,
+	init = function()
+		require("fzf-lua").register_ui_select()
+		vim.g.zoxide_use_select = 1
+	end,
 }
