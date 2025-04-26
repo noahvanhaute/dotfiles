bcd2fc1256d89d99f8869320d01a7d041f9be2b7
noahvanhaute <noah.vanhaute@proton.me>
2025-04-26 15:04:18 +0200

    make fzf-lua use colorscheme

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -2,4 +2,10 @@ return {
 	"ibhagwan/fzf-lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {},
+	config = function()
+		local fzf = require("fzf-lua")
+		fzf.setup({
+			fzf_colors = true,
+		})
+	end,
 }
