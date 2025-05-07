c46507d01783a30ee1a2d2d4e96e70b81b7f1c1f
noahvanhaute <noah.vanhaute@proton.me>
2025-05-07 12:00:07 +0200

    make typst preview open in new window

--- .config/nvim/lua/plugins/typst-preview-nvim.lua
+++ .config/nvim/lua/plugins/typst-preview-nvim.lua
@@ -1,8 +1,11 @@
+local is_wsl = vim.fn.has("wsl") == 1
+
 return {
 	"chomosuke/typst-preview.nvim",
 	ft = "typst",
 	version = "1.*",
 	opts = {
 		dependencies_bin = { ["tinymist"] = "tinymist" },
+		open_cmd = is_wsl and "firefox.exe --new-window %s" or "firefox --new-window %s",
 	},
 }
