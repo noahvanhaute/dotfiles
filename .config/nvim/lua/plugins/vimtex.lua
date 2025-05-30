8caab0e110c67aeb079bb1c850e495a8a11caba7
noahvanhaute <noah.vanhaute@proton.me>
2025-05-30 14:52:18 +0200

    disable vimtex conceal for spacing commands

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -9,6 +9,23 @@ return {
 		-- don't open QuickFix for warnings only
 		vim.g.vimtex_quickfix_open_on_warning = false
 
+		-- disable conceal for spacing commands
+		vim.g.vimtex_syntax_conceal = {
+			["accents"] = 1,
+			["ligatures"] = 1,
+			["cites"] = 1,
+			["fancy"] = 1,
+			["spacing"] = 0,
+			["greek"] = 1,
+			["math_bounds"] = 1,
+			["math_delimiters"] = 1,
+			["math_fracs"] = 1,
+			["math_super_sub"] = 1,
+			["math_symbols"] = 1,
+			["sections"] = 0,
+			["styles"] = 1,
+		}
+
 		-- fzf-lua for ToC
 		vim.keymap.set("n", "<localleader>lt", function()
 			return require("vimtex.fzf-lua").run()
