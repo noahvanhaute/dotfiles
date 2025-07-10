0deeb735a4c3dc74b10cf5a1d55058cabfa085d5
noahvanhaute <noah.vanhaute@proton.me>
2025-07-10 14:35:12 +0200

    Have Neovim markdown rendering cooperate with nabla

--- .config/nvim/lua/plugins/render-markdown-nvim.lua
+++ .config/nvim/lua/plugins/render-markdown-nvim.lua
@@ -3,5 +3,16 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = {},
+	opts = {
+		latex = { enabled = false },
+		win_options = { conceallevel = { rendered = 2 } },
+		on = {
+			render = function()
+				require("nabla").enable_virt()
+			end,
+			clear = function()
+				require("nabla").disable_virt()
+			end,
+		},
+	},
 }
