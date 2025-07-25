c128e3029cadb49cf1d775771a6d6dd12231d0e9
noahvanhaute <noah.vanhaute@proton.me>
2025-07-25 15:00:00 +0200

    Return to last edit position when opening files with Neovim

--- .config/nvim/lua/config/nvim-options.lua
+++ .config/nvim/lua/config/nvim-options.lua
@@ -113,8 +113,21 @@ vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase w
 -- highlight text being yanked
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
 	callback = function()
 		vim.hl.on_yank()
+	end,
+})
+
+-- return to last edit position when opening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+	desc = "return to last edit position when opening files",
+	group = vim.api.nvim_create_augroup("return-position", { clear = true }),
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
 	end,
 })
