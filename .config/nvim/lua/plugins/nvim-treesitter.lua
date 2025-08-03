cebaacbdff1bde7b6bd541e499fde6ea62c235d5
noahvanhaute <noah.vanhaute@proton.me>
2025-08-03 16:05:28 +0200

    Removed unused tree-sitter incremental selection

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -38,15 +38,6 @@ return {
 				"vimdoc",
 				"yaml",
 			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
 		})
 	end,
 }
