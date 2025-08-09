8f6099d773d24ade33c2c1ac4ec1db8c78cdb6ca
noahvanhaute <noah.vanhaute@proton.me>
2025-08-09 19:21:35 +0200

    Fixed weird spell highlights for solarized.nvim

--- .config/nvim/lua/plugins/solarized.lua
+++ .config/nvim/lua/plugins/solarized.lua
@@ -6,6 +6,16 @@ return {
 	opts = {},
 	config = function()
 		require("solarized").setup({
+			on_highlights = function(colors)
+				---@type solarized.highlights
+				local groups = {
+					SpellBad = { underline = false, strikethrough = false, sp = colors.red, undercurl = true },
+					SpellCap = { sp = colors.violet, undercurl = true },
+					SpellLocal = { sp = colors.yellow, undercurl = true },
+					SpellRare = { sp = colors.cyan, undercurl = true },
+				}
+				return groups
+			end,
 			transparent = {
 				enabled = true,
 			},
