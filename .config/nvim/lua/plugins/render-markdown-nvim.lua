0922c10d1e028b3f79103b3f7a0fb411885f4b11
noahvanhaute <noah.vanhaute@proton.me>
2025-08-20 10:57:20 +0200

    Disabled html functionality in render-markdown

--- .config/nvim/lua/plugins/render-markdown-nvim.lua
+++ .config/nvim/lua/plugins/render-markdown-nvim.lua
@@ -3,5 +3,5 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = { latex = { enabled = false } },
+	opts = { html = { enabled = false }, latex = { enabled = false } },
 }
