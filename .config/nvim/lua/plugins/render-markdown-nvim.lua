ef79b6d28f262dfafd2556a54391a8a81c3381b7
noahvanhaute <noah.vanhaute@proton.me>
2025-09-06 10:26:27 +0200

    Moved some render-markdown settings from opts table to config function for checkhealth

--- .config/nvim/lua/plugins/render-markdown-nvim.lua
+++ .config/nvim/lua/plugins/render-markdown-nvim.lua
@@ -3,13 +3,15 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 	---@module 'render-markdown'
 	---@type render.md.UserConfig
-	opts = { html = { enabled = false }, latex = { enabled = false } },
+
 	config = function()
 		vim.api.nvim_set_hl(0, "@markup.link.label.markdown_inline", { link = "Conceal", underline = true })
 		vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", { link = "Normal" })
 
 		require("render-markdown").setup({
 			code = { width = "block", min_width = 90 },
+			html = { enabled = false },
+			latex = { enabled = false },
 			sign = { enabled = false },
 		})
 	end,
