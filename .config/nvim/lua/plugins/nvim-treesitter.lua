5b8939ab6ecbbca02258917641afe6b52821cb36
noahvanhaute <noah.vanhaute@proton.me>
2025-09-22 23:16:28 +0200

    Specified nvim-treesitter branch as is recommended

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	event = { "BufReadPre", "BufNewFile" },
+	branch = "master",
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
