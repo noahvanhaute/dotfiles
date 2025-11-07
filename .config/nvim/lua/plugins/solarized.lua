3996e381990376317a4dbba53e7495d06d017c5c
noahvanhaute <noah.vanhaute@proton.me>
2025-11-07 20:06:25 +0100

    Toned down color scheme

--- .config/nvim/lua/plugins/solarized.lua
+++ .config/nvim/lua/plugins/solarized.lua
@@ -9,10 +9,17 @@ return {
 			on_highlights = function(colors)
 				---@type solarized.highlights
 				local groups = {
+					Constant = { fg = colors.blue },
+					Function = { fg = colors.base0 },
+					Identifier = { fg = colors.base0 },
+					Parameter = { fg = colors.base0 },
+					Property = { fg = colors.base0 },
 					SpellBad = { underline = false, strikethrough = false, sp = colors.red, undercurl = true },
 					SpellCap = { sp = colors.violet, undercurl = true },
 					SpellLocal = { sp = colors.yellow, undercurl = true },
 					SpellRare = { sp = colors.cyan, undercurl = true },
+					Statement = { fg = colors.base0 },
+					Type = { fg = colors.base0 },
 				}
 				return groups
 			end,
