9ede7e100b5f53a10ef0649e9338931781c89b35
noahvanhaute <noah.vanhaute@proton.me>
2025-12-30 16:51:28 +0100

    Fixed small error in comment

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,6 +1,6 @@
 vim.treesitter.start()
 
--- Turn off spell for floating windows so that `vim.lsp.buf.hover()
+-- Turn off spell for floating windows so that `vim.lsp.buf.hover()`
 --  isn't spellchecked
 vim.opt_local.spell = not vim.api.nvim_win_get_config(0).zindex
 
