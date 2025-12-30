c14066be0d164ead223b8ca198fa79b94c4f9db4
noahvanhaute <noah.vanhaute@proton.me>
2025-12-30 14:11:19 +0100

    Lazy update, switch to new branch for nvim-treesitter

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,21 +1,16 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	event = { "BufReadPre", "BufNewFile" },
-	branch = "master",
+	lazy = false,
+	branch = "main",
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			highlight = { enable = true },
-			indent = { enable = true },
-			ensure_installed = {
-				"bash",
-				"gitignore",
-				"json",
-				"markdown_inline",
-				"ron",
-				"vim",
-				"yaml",
-			},
+		require("nvim-treesitter").install({
+			"bash",
+			"gitignore",
+			"json",
+			"ron",
+			"yaml",
 		})
 	end,
 }
