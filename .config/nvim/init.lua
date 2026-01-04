fd1dbfa5478f3cc0e65de17c636453df4df162a7
noahvanhaute <noah.vanhaute@proton.me>
2026-01-04 19:28:49 +0100

    Remove lsp highlighting of word under cursor in Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -121,33 +121,3 @@ require("lazy").setup({
 vim.lsp.enable({ "ltex_plus", "lua_ls", "tinymist" })
 
 vim.diagnostic.config({ virtual_lines = { current_line = true } })
-
--- Highlight references of the word under the cursor
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
-	callback = function(event)
-		local client = vim.lsp.get_client_by_id(event.data.client_id)
-		if client and client.supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-			local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
-			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-				buffer = event.buf,
-				group = highlight_augroup,
-				callback = vim.lsp.buf.document_highlight,
-			})
-
-			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-				buffer = event.buf,
-				group = highlight_augroup,
-				callback = vim.lsp.buf.clear_references,
-			})
-
-			vim.api.nvim_create_autocmd("LspDetach", {
-				group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
-				callback = function(event2)
-					vim.lsp.buf.clear_references()
-					vim.api.nvim_clear_autocmds({ group = "lsp-highlight", buffer = event2.buf })
-				end,
-			})
-		end
-	end,
-})
