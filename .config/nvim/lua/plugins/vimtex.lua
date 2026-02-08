584c8310ef1dc022ed0e55c24f84726c29af887c
noahvanhaute <noah.vanhaute@proton.me>
2026-02-08 14:28:00 +0100

    Turn off conceal for tex/latex

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -4,21 +4,7 @@ return {
 
 	init = function()
 		vim.g.vimtex_quickfix_open_on_warning = false
-		vim.g.vimtex_syntax_conceal = {
-			["accents"] = 1,
-			["ligatures"] = 1,
-			["cites"] = 1,
-			["fancy"] = 1,
-			["spacing"] = 0,
-			["greek"] = 1,
-			["math_bounds"] = 1,
-			["math_delimiters"] = 1,
-			["math_fracs"] = 1,
-			["math_super_sub"] = 1,
-			["math_symbols"] = 1,
-			["sections"] = 0,
-			["styles"] = 1,
-		}
+
 		vim.g.vimtex_view_method = "zathura_simple"
 
 		-- Use fzf-lua for ToC
