d806a7e203fe56050f1ea19c0d2bf3715e53b420
noahvanhaute <noah.vanhaute@proton.me>
2026-05-13 22:24:17 +0200

    Remove fzf-lua

--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -3,15 +3,9 @@ return {
 	lazy = false,
 
 	init = function()
-		vim.g.vimtex_syntax_nospell_comments = true
-
 		vim.g.vimtex_quickfix_open_on_warning = false
-
+		vim.g.vimtex_syntax_nospell_comments = true
 		vim.g.vimtex_view_method = "zathura_simple"
-
-		vim.keymap.set("n", "<localleader>lt", function()
-			return require("vimtex.fzf-lua").run()
-		end, { desc = "Seach through table of contents with fzf-lua" })
 
 		-- Cleanup on quit
 		vim.api.nvim_create_autocmd("User", {
